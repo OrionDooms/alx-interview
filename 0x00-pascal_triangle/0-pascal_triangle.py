f36940cb7161dfbffef23dd43211f5bdb28c3c70
Orion <orion45.dd@gmail.com>
2024-08-08 13:38:29 -0700

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,30 +3,26 @@
 The script to Pascal's Triangle.
 """
 
-'''Pascal's Triangle - Returns a lists of integers.
-n will be always an integer.
-Returns an empty list if n = 0.'''
-
 
 def pascal_triangle(n):
+    """
+    Pascal's Triangle - Returns a lists of integers.
+    n will be always an integer.
+    Returns an empty list if n = 0.
+    """
     triangle = []
+    if n <= 0:
+        return triangle
     for i in range(n):
-        row = []
-        for j in range(i + 1):
-            row.append(Xfactorial(i) // (Xfactorial(j) * Xfactorial(i - j)))
+
+        """ start each row with 1s """
+        row = [1] * (i + 1)
+
+        """ Update the values in between """
+        for j in range(1, i):
+            tempA = triangle[i - 1][j - 1]
+            tempB = triangle[i - 1][j]
+            row[j] = tempA + tempB
+
         triangle.append(row)
     return triangle
-
-
-"""
-Factural is a multiplication operation of natural numbers.
-n! = n * (n - 1) * (n - 2)
-"""
-
-
-def Xfactorial(n):
-    result = 1
-    """ Multiply all numbers from 1 to n"""
-    for i in range(2, n + 1):
-        result *= i
-    return result
