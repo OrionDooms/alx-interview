8a05d5de7a26237bbab799323676eabdec52f5a7
Orion <Orion45.dd@gmail.com>
2024-09-17 06:36:12 -0700

    api Starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/{}/`.format(movieId);
+
+const movieData = JSON.parse(body);
+
+movieData.characters.forEach((characterUrl) => {
+	request(characterUrl, (charError, charResponse, charBody) => {
+		if (charError){
+			console.log("Error:", charError);
+			return;
+		}
+		const characterData = JSON.parse(charBody);
+		console.log(characterData.name);
+	});
+});
