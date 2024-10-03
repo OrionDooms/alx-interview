8dacb4d0c369078fdc9e1735d9fe9b6c89f7f880
Orion <Orion45.dd@gmail.com>
2024-10-03 22:00:42 +0000

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Making Change function"""
+
+
+def makeChange(coins, total):
+    """makeChange takes a pile of coins of different values and determine
+    the fewest number of coins needed to meet a given amount total"""
+    if total >= 0:
+        fewest_coins = float('inf')
+        for coin in coins:
+            if amount - coin >= 0:
+                value = MinCoins(coins, amount - coin)
+                if value >= 0:
+                    result = value + 1
+                    fewest_coins = min(fewest_coins, result)
+        if fewest_coins != flaot('inf'):
+            return fewest_coins
+        else:
+            return -1
+    return 0
