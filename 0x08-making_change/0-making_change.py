4ba5d33270beb76805ee9652d0270176ccc87f78
Orion <Orion45.dd@gmail.com>
2024-10-06 21:36:29 +0000

    updated, added recursive approach

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,21 +2,35 @@
 """Making Change function"""
 
 
-def makeChange(coins, total):
-    """makeChange takes a pile of coins of different values and determine
-    the fewest number of coins needed to meet a given amount total"""
+def ChangeHelper(coins, total, m):
+    """ChangeHelper use recursion to handle larger coin values.
+    The memo(Memoization) helps store already computed results and avoiding
+    repeated calculated totals"""
     if total != 0:
         if total < 0:
             return -1
+        """check memo is already memoized"""
+        if total in m:
+            return m[total]
+        """Initialize the fewest_coins to infinity"""
         fewest_coins = float('inf')
         for coin in coins:
-            if amount - coin >= 0:
-                value = MinCoins(coins, amount - coin)
+            if total >= coin:
+                value = ChangeHelper(coins, total - coin, m)
                 if value >= 0:
                     result = value + 1
                     fewest_coins = min(fewest_coins, result)
-        if fewest_coins != flaot('inf'):
-            return fewest_coins
+        """The current memo total"""
+        if fewest_coins != float('inf'):
+            m[total] = fewest_coins
         else:
-            return -1
+            m[total] = -1
+        return m[total]
     return 0
+
+
+def makeChange(coins, total):
+    """makeChange takes a pile of coins of different values and determine
+    the fewest number of coins needed to meet a given amount total"""
+    memo = {}
+    return ChangeHelper(coins, total, memo)
