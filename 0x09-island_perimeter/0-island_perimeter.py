10ec034caac8fd34d64923bf155be0568fd6b511
Orion <Orion45.dd@gmail.com>
2024-10-10 19:59:19 +0000

    update

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,18 +6,33 @@ def island_perimeter(grid):
     """island_perimeter(grid) returns the perimeter of the island
     The island grid is a list of list of integers, 0 is the water,
     and 1 is the land"""
+    if type(grid) != list:
+        return 0
+    row = len(grid)
+    col = len(grid[0])
     result = 0
-    if type(grid) == list:
-        rows = len(grid)
-        cols = len(grid[0])
-        """loop through each cell in the grid"""
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == 1:
-                    result += 4
-                    """check the top, bottom, left side and right side"""
-                    if (grid[row + 1][col] == 1 and row < rows - 1):
-                        result -= 2
-                    if (grid[row][col + 1] == 1 and col < cols - 1):
-                        result -= 2
+    """loop through each cell in the grid"""
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] == 1:
+                """check the top, bottom, left side and right side"""
+                if grid[i - 1][j] == 0 or i == 0:
+                    result += 1
+                else:
+                    result += 0
+
+                if grid[i + 1][j] == 0 or i == row - 1:
+                    result += 1
+                else:
+                    result += 0
+
+                if grid[i][j - 1] == 0 or j == 0:
+                    result += 1
+                else:
+                    result += 0
+
+                if grid[i][j + 1] == 0 or j == col - 1:
+                    result += 1
+                else:
+                    result += 0
     return result
