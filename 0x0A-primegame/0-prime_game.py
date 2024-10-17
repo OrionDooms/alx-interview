7ca8a0ce9ca658e0f891560d7aee7313c46304c0
Orion <Orion45.dd@gmail.com>
2024-10-17 12:13:49 +0000

    prime number game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,39 +1,46 @@
 #!/usr/bin/python3
+"""Prime Game"""
+
 
 def checkForPrime(n):
+    """This function is to find the prime numbers, given number n and
+    counting how many primes exist at each number"""
     if n < 2:
         return [0] * (n + 1)
-
-    is_prime = [True] * (n + 1)
-    is_prime[0] = False
-    is_prime[1] = False
+    """prime_check is an array each index represents a if number is prime.
+    All numbers are prime, set to True, except for 0 and 1, which are set to
+    False"""
+    prime_check = [True] * (n + 1)
+    prime_check[0] = False
+    prime_check[1] = False
     prime_count = [0] * (n + 1)
-    count = 0
+    a = 0
 
     for i in range(2, n + 1):
-        if is_prime[i]:
-            count += 1
+        if prime_check[i]:
+            a += 1
             for j in range(i * i, n + 1, i):
-                is_prime[j] = False
-        prime_count[i] = count
-        
+                prime_check[j] = False
+        prime_count[i] = a
     return prime_count
 
 
 def isWinner(x, nums):
-      if x > 0 or nums:
-          max_n = max(nums)
-          prime_count = checkForPrime(max_n)
-          maria = 0
-          ben = 0
-          for n in nums:
-              if prime_count[n] % 2 == 1:
-                  maria += 1
-              else:
-                  ben += 1
-          if maria > ben:
-              return "Maria"
-          else:
-              return "Ben"
-      
-      return None
+    """This function determines who wins between Maria and Ben."""
+    if x > 0 and nums:
+        max_n = max(nums)
+        prime_count = checkForPrime(max_n)
+        maria = 0
+        ben = 0
+        for n in nums:
+            if prime_count[n] % 2 == 1:
+                maria += 1
+            else:
+                ben += 1
+        if maria > ben:
+            return "Maria"
+        elif ben > maria:
+            return "Ben"
+        else:
+            return None
+    return None
